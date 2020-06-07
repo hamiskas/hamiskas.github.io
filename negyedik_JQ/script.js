5103f2f5227d5efe1c819cbd501ae04042b1a3b9
hamiskas <zsozsi@webmail.hu>
2020-06-07 18:17:40 +0200

    JQ_1-2
    JQuery_1.2

--- negyedik_JQ/script.js
+++ negyedik_JQ/script.js
@@ -2,18 +2,18 @@ let color = 'purple';
 let number = 10;
 let word = 'cool';
 if (color === 'purple') {
-   $('#North').css('background', 'purple');
+   $('.north').css('background', 'purple');
 };
 if(number > 100) {
-   $('#East').text("whoah that's a big number.");
+   $('.east').text("whoah that's a big number.");
 }
 else {
-   $('#East').text("just a regular number, please.");
+   $('.east').text("just a regular number, please.");
 };
 
 if(word === 'cool') {
-   $('#South').text('Power of DOM');
+   $('.south').text('Power of DOM');
 }
 else {
-   $('#West').text('Power of DOM')
+   $('.west').text('Power of DOM')
 };
