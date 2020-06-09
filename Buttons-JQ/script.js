c439615f3400c573c5eadd65f1c1d9ab57b59ed8
hamiskas <zsozsi@webmail.hu>
2020-06-10 00:17:50 +0200

    JQ-6
    JQuery_6

--- Buttons-JQ/script.js
+++ Buttons-JQ/script.js
@@ -1,9 +1,17 @@
+let clickeD = 0;
 $('.firsT').on('click', (event) => {
      console.log('Yeah, you clicked me') 
 });
 $('.seconD').on('click', (event) => {
     $('.firsT').text('Clicked_2nd')
 });
-$('.thirD').on('click', (event) => {
-    $('button').css('background', 'red')
+$('.thirD') .on('click', (event) => {
+   if (clickeD === 0) {
+      //var color = document.getElementById('coloR').value;
+       var color = $('#coloR') .val();
+       $('button') .css('background', `${color}`)
+       $('button.thirD') .css('background', 'lightgrey');
+    }
+    $('button.thirD') .text("Out_of_order: "+clickeD+"x");
+    clickeD++;
 });
